a165ed64066d00f5777ee363c66260286d531c79
Paulo Henrique Tavares de Andrade <ph.and@hotmail.com>
2023-02-21 10:41:55 +0000

    header component added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheHeader />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import TheHeader from './layout/TheHeader.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    components: {
+      TheHeader
+    },
+    data() {
+      return {
+      };
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  html {
+    font-family: sans-serif;
+  }
+
+  body {
+    margin: 0;
+    min-height: 100vh;
+  }
 </style>
---
+++ src/layout/TheHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <slot>
+      <h1>RememberMe</h1>
+    </slot>
+  </header>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style scoped>
+  header {
+    background-color: rgba(96, 10, 43, 1);
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    color: rgba(255, 255, 255, 1);
+  }
+</style>
